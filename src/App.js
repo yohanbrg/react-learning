2b692add9e91182681976d7284ae41b77a052acc
Yohan Bourgogne <ybourgogne@tequilarapido.com>
2019-03-15 13:37:17 +0100

    Experiment state

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Table from './Table';
 
 class App extends Component{
 
-    render() {
-        const characters = [
+    state = {
+        characters: [
             {
                 'name': 'Charlie',
                 'job': 'Janitor'
@@ -22,10 +22,30 @@ class App extends Component{
                 'name': 'Dennis',
                 'job': 'Bartender'
             }
-        ];
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
+
+    render() {
+        
+        const { characters } = this.state;
+
         return (
             <div className="container">
-                <Table characterData={characters} />
+                <Table 
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter} 
+                />
             </div>
         );
     }
--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ const TableHeader = () => {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Action</th>
             </tr>
         </thead>
     )
@@ -17,6 +18,11 @@ const TableBody = props => {
           <tr key={index}>
             <td>{row.name}</td>
             <td>{row.job}</td>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>
+                    Delete
+                </button>
+            </td>
           </tr>
         );
     });
@@ -26,12 +32,15 @@ const TableBody = props => {
 
 class Table extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData, removeCharacter } = this.props;
 
         return (
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData} />
+                <TableBody 
+                    characterData={characterData}
+                    removeCharacter={removeCharacter} 
+                />
             </table>
         );
     }
