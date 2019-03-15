8e373555d8f780d908384e8a8d0c81956df4ad20
Yohan Bourgogne <ybourgogne@tequilarapido.com>
2019-03-15 14:31:19 +0100

    add form to manage table entries

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, {Component} from 'react';
 
 import Table from './Table';
+import Form from './Form';
 
 class App extends Component{
 
     state = {
-        characters: [
-            {
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bouncer'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ]
+        characters: []
     };
 
     removeCharacter = index => {
@@ -35,6 +19,13 @@ class App extends Component{
         });
     }
 
+    handleSubmit = character => {
+        this.setState(
+            {
+                characters: [...this.state.characters, character]
+            }
+        );
+    }
 
     render() {
         
@@ -46,6 +37,7 @@ class App extends Component{
                     characterData={characters}
                     removeCharacter={this.removeCharacter} 
                 />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         );
     }
